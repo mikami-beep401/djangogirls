3080045b1248c96d44da60d14cbb172ccf700b66
Mikami <miamia1124@gmail.com>
2025-09-23 16:35:43 +0000

    html

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,3 +21,11 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
